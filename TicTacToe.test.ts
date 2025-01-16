50907e230ee44d7139353156b1132ada57a02035
Bruno <brunokoppel.dev@gmail.com>
2025-01-16 12:33:26 -0500

    Added .gitignore, TicTacToe class methods

--- TicTacToe.test.ts
+++ TicTacToe.test.ts
@@ -3,15 +3,35 @@ import { TicTacToe } from './TicTacToe';
 
 let boardHorizontalWin = [
   ['x', 'x', 'x', 'x'], 
+  ['o', 'o', 'o', 'x'], 
+  ['x', 'o', 'x', 'o'], 
+  ['o', 'x', 'o', 'o']
+];
+
+let boardVerticalWin = [
   ['o', 'x', 'o', 'x'], 
+  ['o', 'o', 'o', 'x'], 
   ['x', 'o', 'x', 'x'], 
+  ['o', 'x', 'o', 'x']
+];
+
+let boardDiagonalWin = [
+  ['o', 'x', 'o', 'x'], 
+  ['o', 'o', 'x', 'o'], 
+  ['x', 'x', 'o', 'o'], 
   ['x', 'x', 'o', 'o']
 ];
 
-describe('Horizontal win', () => {
-  it('Should be horizontal', () => {
+describe('Winning solutions', () => {
+  it('Horizontal Win', () => {
     const game = new TicTacToe();
 
-    expect(game.checkWinner(boardHorizontalWin)).toBe(true);
+    expect(game.checkWinner(boardHorizontalWin)).toBe("winner");
+  });
+
+  it('Vertical Win', () => {
+    const game = new TicTacToe();
+
+    expect(game.checkWinner(boardVerticalWin)).toBe("winner");
   });
 });
--- TicTacToe.ts
+++ TicTacToe.ts
@@ -2,7 +2,39 @@ export class TicTacToe {
   public board: string[][];
 
   public checkWinner(board: string[][]): string { 
-    return 'yes';
+    for (let i = 0; i < 4; i++) {
+
+      // Horizontal Check
+      if (board[i][0] === board[i][1] && board[i][0] === board[i][2] && board[i][0] === board[i][3]) {
+        return 'winner';
+      }
+
+      // Vertical Check
+      if (board[0][i] === board[1][i] && board[0][i] === board[2][i] && board[0][i] === board[3][i]) {
+        return 'winner';
+      }
+
+      // Diagonal Check
+      if (i == 0 && board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === board[3][3]) {
+        return 'winner';
+      }
+
+      if (i == 4 && board[0][3] === board[1][2] && board[0][3] === board[2][1] && board[0][3] === board[3][0]) {
+        return 'winner';
+      }
+
+
+      // 2x2 check
+      if (i < 3) {
+        for (let j = 0; j < 3; j++) {
+          if (this.checkSquare(board, i, j)) {
+            return 'winner';
+          }
+        }
+      }
+    }
+
+    return 'no';
   }
 
   public anyMovesLeft(board: string[][]): boolean { 
@@ -11,6 +43,14 @@ export class TicTacToe {
 
   public isGameOver(board: string[][]): boolean { 
     return true;
+  }
+
+  public checkSquare(board: string[][], row: number, col: number): boolean {
+    if (board[row][col] === board[row][col + 1] && board[row][col] === board[row + 1][col] && board[row][col] === board[row + 1][col + 1]) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
 
