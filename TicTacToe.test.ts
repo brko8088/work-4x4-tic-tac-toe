22e9fa43860bdc124f85265407d0f19fb5c7178b
Bruno <brunokoppel.dev@gmail.com>
2025-01-17 11:26:15 -0500

    Added further logic to each class method

--- TicTacToe.test.ts
+++ TicTacToe.test.ts
@@ -1,5 +1,24 @@
-// sum.test.ts
 import { TicTacToe } from './TicTacToe';
+
+let emptyBoard = [
+  ['', '', '', ''], 
+  ['', '', '', ''], 
+  ['', '', '', ''], 
+  ['', '', '', '']
+];
+let noMovesLeft = [
+  ['o', 'x', '', 'x'], 
+  ['x', 'o', 'x', 'o'], 
+  ['x', 'o', 'x', 'o'], 
+  ['o', 'x', 'o', 'x']
+];
+
+let anyMovesLeft = [
+  ['o', 'x', '', ''], 
+  ['x', 'o', 'x', 'o'], 
+  ['x', 'x', 'o', 'o'], 
+  ['x', 'o', 'x', 'o']
+]
 
 let boardHorizontalWin = [
   ['x', 'x', 'x', 'x'], 
@@ -17,21 +36,46 @@ let boardVerticalWin = [
 
 let boardDiagonalWin = [
   ['o', 'x', 'o', 'x'], 
-  ['o', 'o', 'x', 'o'], 
+  ['x', 'o', 'x', 'o'], 
   ['x', 'x', 'o', 'o'], 
-  ['x', 'x', 'o', 'o']
+  ['x', 'o', 'x', 'o']
+];
+
+let boardSquareWin = [
+  ['o', 'o', 'x', 'x'],
+  ['x', 'o', 'x', 'x'],
+  ['o', 'x', 'x', 'o'],
+  ['o', 'x', 'o', 'x']
 ];
 
 describe('Winning solutions', () => {
   it('Horizontal Win', () => {
     const game = new TicTacToe();
-
-    expect(game.checkWinner(boardHorizontalWin)).toBe("winner");
+    expect(game.checkWinner(boardHorizontalWin)).toBe("horizontal win");
   });
 
   it('Vertical Win', () => {
     const game = new TicTacToe();
+    expect(game.checkWinner(boardVerticalWin)).toBe("vertical win");
+  });
 
-    expect(game.checkWinner(boardVerticalWin)).toBe("winner");
+  it('Diagonal Win', () => {
+    const game = new TicTacToe();
+    expect(game.checkWinner(boardDiagonalWin)).toBe("diagonal win");
+  });
+
+  it('2x2 Win', () => {
+    const game = new TicTacToe();
+    expect(game.checkWinner(boardSquareWin)).toBe("2x2 win");
+  });
+
+  it('There are moves left', () => {
+    const game = new TicTacToe();
+    expect(game.anyMovesLeft(emptyBoard)).toBe(true);
+  });
+
+  it('There are no moves left', () => {
+    const game = new TicTacToe();
+    expect(game.anyMovesLeft(noMovesLeft)).toBe(false);
   });
 });
--- TicTacToe.ts
+++ TicTacToe.ts
@@ -1,3 +1,10 @@
+let emptyBoard = [
+  ['', '', '', ''], 
+  ['', '', '', ''], 
+  ['', '', '', ''], 
+  ['', '', '', '']
+];
+
 export class TicTacToe {
   public board: string[][];
 
@@ -6,29 +13,28 @@ export class TicTacToe {
 
       // Horizontal Check
       if (board[i][0] === board[i][1] && board[i][0] === board[i][2] && board[i][0] === board[i][3]) {
-        return 'winner';
+        return 'horizontal win';
       }
 
       // Vertical Check
       if (board[0][i] === board[1][i] && board[0][i] === board[2][i] && board[0][i] === board[3][i]) {
-        return 'winner';
+        return 'vertical win';
       }
 
       // Diagonal Check
       if (i == 0 && board[0][0] === board[1][1] && board[0][0] === board[2][2] && board[0][0] === board[3][3]) {
-        return 'winner';
+        return 'diagonal win';
       }
 
       if (i == 4 && board[0][3] === board[1][2] && board[0][3] === board[2][1] && board[0][3] === board[3][0]) {
-        return 'winner';
+        return 'diagonal win';
       }
-
 
       // 2x2 check
       if (i < 3) {
         for (let j = 0; j < 3; j++) {
-          if (this.checkSquare(board, i, j)) {
-            return 'winner';
+          if (board[i][j] === board[i][j + 1] && board[i][j] === board[i + 1][j] && board[i][j] === board[i + 1][j + 1]) {
+            return '2x2 win';
           }
         }
       }
@@ -38,15 +44,53 @@ export class TicTacToe {
   }
 
   public anyMovesLeft(board: string[][]): boolean { 
-    return true;
+    for (let i = 0; i < 4; i++) {
+      // Horizontal Check
+      if (board[i][0] !== '' ||  board[i][1] === '' || board[i][2] === '' || board[i][3] === '') {
+        return true;
+      }
+
+      // Vertical Check
+      if (board[0][i] === '' ||  board[1][i] === '' || board[2][i] === '' || board[3][i] === '') {
+        return true;
+      }
+
+      // Diagonal Check
+      if (i == 0 && board[0][0] === '' || board[1][1] === '' || board[2][2] === '' || board[3][3] === '') {
+        return true;
+      }
+
+      // 2x2 check
+      if (i < 3) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] === '' || board[i][j + 1] === '' || board[i + 1][j] === '' || board[i + 1][j + 1] === '') {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  public anyMovesLeftForWin(characterToFind: string, characters: string[]): boolean {
+    let emptySpaceFound = false;
+
+    for (let i = 0; i < characters.length; i++) {
+      if (characters[i] === '') {
+        emptySpaceFound = true;
+      }
+
+      if (characters[i] !== characterToFind) {
+        return true;
+      }
+    }
+
+    return emptySpaceFound;
   }
 
   public isGameOver(board: string[][]): boolean { 
-    return true;
-  }
-
-  public checkSquare(board: string[][], row: number, col: number): boolean {
-    if (board[row][col] === board[row][col + 1] && board[row][col] === board[row + 1][col] && board[row][col] === board[row + 1][col + 1]) {
+    if (this.checkWinner(board) !== 'no' || !this.anyMovesLeft(board)) {
       return true;
     } else {
       return false;
