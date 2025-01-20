b6634c475e4794084194d071ea0c9e9afc965846
Bruno <brunokoppel.dev@gmail.com>
2025-01-20 13:43:07 -0500

    Running lint on files

--- TicTacToe.test.ts
+++ TicTacToe.test.ts
@@ -1,333 +1,331 @@
-import { TicTacToe } from './TicTacToe';
-import { 
-  boardHorizontalWin, 
-  boardVerticalWin, 
+import { TicTacToe } from "./TicTacToe";
+import {
+  boardHorizontalWin,
+  boardVerticalWin,
   boardDiagonalWinOne,
   boardDiagonalWinTwo,
-  boardCornersWin, 
-  boardSquareWin, 
-  emptyBoard, 
-  noMovesLeftOne, 
-  noMovesLeftTwo, 
-  movesLeftOne, 
-  movesLeftTwo, 
-  movesLeftThree, 
+  boardCornersWin,
+  boardSquareWin,
+  emptyBoard,
+  noMovesLeftOne,
+  noMovesLeftTwo,
+  movesLeftOne,
+  movesLeftTwo,
+  movesLeftThree,
   movesLeftFour,
   movesLeftFive,
   movesLeftSix,
   movesLeftSeven,
-  movesLeftEight
-} from './Boards';
+  movesLeftEight,
+} from "./Boards";
 
-describe('Test checkWinner Method', () => {
-  it('Horizontal Win', () => {
+describe("Test checkWinner Method", () => {
+  it("Horizontal Win", () => {
     const game = new TicTacToe(boardHorizontalWin);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('horizontal');
-    expect(result.character).toBe('x'); 
+    expect(result.type).toBe("horizontal");
+    expect(result.character).toBe("x");
   });
 
-  it('Vertical Win', () => {
+  it("Vertical Win", () => {
     const game = new TicTacToe(boardVerticalWin);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('vertical');
-    expect(result.character).toBe('x'); 
+    expect(result.type).toBe("vertical");
+    expect(result.character).toBe("x");
   });
 
-  it('Diagonal Win 1', () => {
+  it("Diagonal Win 1", () => {
     const game = new TicTacToe(boardDiagonalWinOne);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('diagonal');
-    expect(result.character).toBe('o'); 
+    expect(result.type).toBe("diagonal");
+    expect(result.character).toBe("o");
   });
 
-  
-  it('Diagonal Win 2', () => {
+  it("Diagonal Win 2", () => {
     const game = new TicTacToe(boardDiagonalWinTwo);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('diagonal');
-    expect(result.character).toBe('x'); 
+    expect(result.type).toBe("diagonal");
+    expect(result.character).toBe("x");
   });
 
-  it('Four Corners Win', () => {
+  it("Four Corners Win", () => {
     const game = new TicTacToe(boardCornersWin);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('corners');
-    expect(result.character).toBe('x');
+    expect(result.type).toBe("corners");
+    expect(result.character).toBe("x");
   });
 
-  it('2x2 Win', () => {
+  it("2x2 Win", () => {
     const game = new TicTacToe(boardSquareWin);
     let result = game.checkWinner();
     expect(result.win).toBe(true);
-    expect(result.type).toBe('2x2');
-    expect(result.character).toBe('x'); 
+    expect(result.type).toBe("2x2");
+    expect(result.character).toBe("x");
   });
 
-  it ('Empty Board No Winner', () => {
+  it("Empty Board No Winner", () => {
     const game = new TicTacToe(emptyBoard);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('No Moves Left 1 No Winner', () => {
+  it("No Moves Left 1 No Winner", () => {
     const game = new TicTacToe(noMovesLeftOne);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('No Moves Left 2 No Winner', () => {
+  it("No Moves Left 2 No Winner", () => {
     const game = new TicTacToe(noMovesLeftTwo);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
-
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 1 No Winner', () => {
+  it("There are Moves Left 1 No Winner", () => {
     const game = new TicTacToe(movesLeftOne);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 2 No Winner', () => {
+  it("There are Moves Left 2 No Winner", () => {
     const game = new TicTacToe(movesLeftTwo);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 3 No Winner', () => {
+  it("There are Moves Left 3 No Winner", () => {
     const game = new TicTacToe(movesLeftThree);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 4 No Winner', () => {
+  it("There are Moves Left 4 No Winner", () => {
     const game = new TicTacToe(movesLeftFour);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 5 No Winner', () => {
+  it("There are Moves Left 5 No Winner", () => {
     const game = new TicTacToe(movesLeftFive);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 6 No Winner', () => {
+  it("There are Moves Left 6 No Winner", () => {
     const game = new TicTacToe(movesLeftSix);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 7 No Winner', () => {
+  it("There are Moves Left 7 No Winner", () => {
     const game = new TicTacToe(movesLeftSeven);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 
-  it ('There are Moves Left 8 No Winner', () => {
+  it("There are Moves Left 8 No Winner", () => {
     const game = new TicTacToe(movesLeftEight);
     let result = game.checkWinner();
     expect(result.win).toBe(false);
-    expect(result.type).toBe('');
-    expect(result.character).toBe('');
+    expect(result.type).toBe("");
+    expect(result.character).toBe("");
   });
 });
 
-describe('Test anyMovesLeft Method', () => {
-  it('No Moves Left - Winning Board 1', () => {
+describe("Test anyMovesLeft Method", () => {
+  it("No Moves Left - Winning Board 1", () => {
     const game = new TicTacToe(boardHorizontalWin);
     expect(game.anyMovesLeft()).toBe(false);
   });
-  
-  it('No Moves Left - Winning Board 2', () => {
+
+  it("No Moves Left - Winning Board 2", () => {
     const game = new TicTacToe(boardVerticalWin);
     expect(game.anyMovesLeft()).toBe(false);
   });
-  
-  it('No Moves Left - Winning Board 3', () => {
+
+  it("No Moves Left - Winning Board 3", () => {
     const game = new TicTacToe(boardDiagonalWinOne);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('No Moves Left - Winning Board 4', () => {
+  it("No Moves Left - Winning Board 4", () => {
     const game = new TicTacToe(boardDiagonalWinTwo);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('No Moves Left - Winning Board 5', () => {
+  it("No Moves Left - Winning Board 5", () => {
     const game = new TicTacToe(boardCornersWin);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('No Moves Left - Winning Board 6', () => {
+  it("No Moves Left - Winning Board 6", () => {
     const game = new TicTacToe(boardSquareWin);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('No moves left - No Winner 1', () => {
+  it("No moves left - No Winner 1", () => {
     const game = new TicTacToe(noMovesLeftOne);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('No moves left - No Winner 2', () => {
+  it("No moves left - No Winner 2", () => {
     const game = new TicTacToe(noMovesLeftTwo);
     expect(game.anyMovesLeft()).toBe(false);
   });
 
-  it('There are Moves left - Empty Board', () => {
+  it("There are Moves left - Empty Board", () => {
     const game = new TicTacToe(emptyBoard);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it('There are Moves left 1', () => {
+  it("There are Moves left 1", () => {
     const game = new TicTacToe(movesLeftOne);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it('There are Moves left 2', () => {
+  it("There are Moves left 2", () => {
     const game = new TicTacToe(movesLeftTwo);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it('There are Moves left 3', () => {
+  it("There are Moves left 3", () => {
     const game = new TicTacToe(movesLeftThree);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it ('There are Moves Left 4', () => {
+  it("There are Moves Left 4", () => {
     const game = new TicTacToe(movesLeftFour);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it ('There are Moves Left 5', () => {
+  it("There are Moves Left 5", () => {
     const game = new TicTacToe(movesLeftFive);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it ('There are Moves Left 6', () => {
+  it("There are Moves Left 6", () => {
     const game = new TicTacToe(movesLeftSix);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it ('There are Moves Left 7', () => {
+  it("There are Moves Left 7", () => {
     const game = new TicTacToe(movesLeftSeven);
     expect(game.anyMovesLeft()).toBe(true);
   });
 
-  it ('There are Moves Left 8', () => {
+  it("There are Moves Left 8", () => {
     const game = new TicTacToe(movesLeftEight);
     expect(game.anyMovesLeft()).toBe(true);
   });
 });
 
-describe('Test gameOver Method', () => {
-  it('Game Over - Horizontal Win', () => {
+describe("Test gameOver Method", () => {
+  it("Game Over - Horizontal Win", () => {
     const game = new TicTacToe(boardHorizontalWin);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - Vertical Win', () => {
+  it("Game Over - Vertical Win", () => {
     const game = new TicTacToe(boardVerticalWin);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - Diagonal Win 1', () => {
+  it("Game Over - Diagonal Win 1", () => {
     const game = new TicTacToe(boardDiagonalWinOne);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - Diagonal Win 2', () => {
+  it("Game Over - Diagonal Win 2", () => {
     const game = new TicTacToe(boardDiagonalWinTwo);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - All Four Corners Win', () => {
+  it("Game Over - All Four Corners Win", () => {
     const game = new TicTacToe(boardCornersWin);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - 2x2 Win', () => {
+  it("Game Over - 2x2 Win", () => {
     const game = new TicTacToe(boardSquareWin);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - No moves left - No Winner 1', () => {
+  it("Game Over - No moves left - No Winner 1", () => {
     const game = new TicTacToe(noMovesLeftOne);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('Game Over - No moves left - No Winner 2', () => {
+  it("Game Over - No moves left - No Winner 2", () => {
     const game = new TicTacToe(noMovesLeftTwo);
     expect(game.isGameOver()).toBe(true);
   });
 
-  it('No Game Over - Empty Board', () => {
+  it("No Game Over - Empty Board", () => {
     const game = new TicTacToe(emptyBoard);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it('No Game Over - There are Moves left 1', () => {
+  it("No Game Over - There are Moves left 1", () => {
     const game = new TicTacToe(movesLeftOne);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it('No Game Over - There are Moves left 2', () => {
+  it("No Game Over - There are Moves left 2", () => {
     const game = new TicTacToe(movesLeftTwo);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it('No Game Over - There are Moves left 3', () => {
+  it("No Game Over - There are Moves left 3", () => {
     const game = new TicTacToe(movesLeftThree);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it ('No Game Over - There are Moves Left 4', () => {
+  it("No Game Over - There are Moves Left 4", () => {
     const game = new TicTacToe(movesLeftFour);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it ('No Game Over - There are Moves Left 5', () => {
+  it("No Game Over - There are Moves Left 5", () => {
     const game = new TicTacToe(movesLeftFive);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it ('No Game Over - There are Moves Left 6', () => {
+  it("No Game Over - There are Moves Left 6", () => {
     const game = new TicTacToe(movesLeftSix);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it ('No Game Over - There are Moves Left 7', () => {
+  it("No Game Over - There are Moves Left 7", () => {
     const game = new TicTacToe(movesLeftSeven);
     expect(game.isGameOver()).toBe(false);
   });
 
-  it ('No Game Over - There are Moves Left 8', () => {
+  it("No Game Over - There are Moves Left 8", () => {
     const game = new TicTacToe(movesLeftEight);
     expect(game.isGameOver()).toBe(false);
   });
--- TicTacToe.ts
+++ TicTacToe.ts
@@ -4,26 +4,26 @@ type Result = {
   win: boolean; // true or false
   type: string; // horizontal, vertical, diagonal, corners, 2x2, draw
   character: string; // x or o
-}
+};
 
 export class TicTacToe {
   public board: string[][];
-  public result: Result ;
+  public result: Result;
 
   // Constructor
   constructor(board?: string[][]) {
-    this.board = board ? board : emptyBoard
+    this.board = board ? board : emptyBoard;
     this.result = {
       win: false,
-      type: '',
-      character: ''
-    }
+      type: "",
+      character: "",
+    };
   }
 
   /*
-  * Check if there is a winner
-  */
-  public checkWinner(): Result { 
+   * Check if there is a winner
+   */
+  public checkWinner(): Result {
     for (let i = 0; i < 4; i++) {
       // Horizontal Win
       if (this.winCheck("horizontal", this.board[i]).win) {
@@ -31,150 +31,168 @@ export class TicTacToe {
       }
 
       // Vertical Win
-      if (this.winCheck("vertical", [
-        this.board[0][i], 
-        this.board[1][i], 
-        this.board[2][i], 
-        this.board[3][i]
-      ]).win) {
+      if (
+        this.winCheck("vertical", [
+          this.board[0][i],
+          this.board[1][i],
+          this.board[2][i],
+          this.board[3][i],
+        ]).win
+      ) {
         return this.result;
       }
 
       // 2x2 Win
       if (i < 3) {
         for (let j = 0; j < 3; j++) {
-          if (this.winCheck("2x2", [
-                this.board[i][j], 
-                this.board[i][j + 1], 
-                this.board[i + 1][j], 
-                this.board[i + 1][j + 1]
-              ]).win) {
+          if (
+            this.winCheck("2x2", [
+              this.board[i][j],
+              this.board[i][j + 1],
+              this.board[i + 1][j],
+              this.board[i + 1][j + 1],
+            ]).win
+          ) {
             return this.result;
           }
         }
       }
     }
-    
+
     // Diagonal Wins
-    if (this.winCheck("diagonal", [
-      this.board[0][0], 
-      this.board[1][1], 
-      this.board[2][2], 
-      this.board[3][3]
-    ]).win) {
+    if (
+      this.winCheck("diagonal", [
+        this.board[0][0],
+        this.board[1][1],
+        this.board[2][2],
+        this.board[3][3],
+      ]).win
+    ) {
       return this.result;
     }
 
-    if (this.winCheck("diagonal", [
-      this.board[3][0], 
-      this.board[2][1], 
-      this.board[1][2], 
-      this.board[0][3]
-    ]).win) {
+    if (
+      this.winCheck("diagonal", [
+        this.board[3][0],
+        this.board[2][1],
+        this.board[1][2],
+        this.board[0][3],
+      ]).win
+    ) {
       return this.result;
     }
 
     // All four corners win
-    if (this.winCheck("corners", [
-      this.board[0][0], 
-      this.board[0][3], 
-      this.board[3][0], 
-      this.board[3][3]
-    ]).win) {
+    if (
+      this.winCheck("corners", [
+        this.board[0][0],
+        this.board[0][3],
+        this.board[3][0],
+        this.board[3][3],
+      ]).win
+    ) {
       return this.result;
     }
 
     return this.result;
   }
 
-  public isGameOver(): boolean { 
-    if (this.checkWinner()[0] === 'win' || !this.anyMovesLeft()) {
+  public isGameOver(): boolean {
+    if (this.checkWinner()[0] === "win" || !this.anyMovesLeft()) {
       return true;
     } else {
       return false;
     }
   }
 
-  public anyMovesLeft(): boolean { 
+  public anyMovesLeft(): boolean {
     if (this.checkWinner().win) {
       return false;
     }
 
     let areThereMovesLeft = false;
-    
+
     for (let i = 0; i < 4; i++) {
       // Horizontal Check
-      areThereMovesLeft = this.checkMovesLeftForWin(this.board[i]) || areThereMovesLeft;
+      areThereMovesLeft =
+        this.checkMovesLeftForWin(this.board[i]) || areThereMovesLeft;
 
       // Vertical Check
-      areThereMovesLeft = this.checkMovesLeftForWin([
-        this.board[0][i], 
-        this.board[1][i], 
-        this.board[2][i], 
-        this.board[3][i]
-      ]) || areThereMovesLeft;
+      areThereMovesLeft =
+        this.checkMovesLeftForWin([
+          this.board[0][i],
+          this.board[1][i],
+          this.board[2][i],
+          this.board[3][i],
+        ]) || areThereMovesLeft;
 
       // 2x2 check
       if (i < 3) {
         for (let j = 0; j < 3; j++) {
-          areThereMovesLeft = this.checkMovesLeftForWin([
-            this.board[i][j], 
-            this.board[i][j + 1], 
-            this.board[i + 1][j], 
-            this.board[i + 1][j + 1]
-          ]) || areThereMovesLeft
+          areThereMovesLeft =
+            this.checkMovesLeftForWin([
+              this.board[i][j],
+              this.board[i][j + 1],
+              this.board[i + 1][j],
+              this.board[i + 1][j + 1],
+            ]) || areThereMovesLeft;
         }
       }
     }
 
     // Diagonal Check from 0,0
-    areThereMovesLeft = this.checkMovesLeftForWin([
-      this.board[0][0], 
-      this.board[1][1], 
-      this.board[2][2], 
-      this.board[3][3]
-    ]) || areThereMovesLeft;
-      
+    areThereMovesLeft =
+      this.checkMovesLeftForWin([
+        this.board[0][0],
+        this.board[1][1],
+        this.board[2][2],
+        this.board[3][3],
+      ]) || areThereMovesLeft;
+
     // Diagonal Check from 3,0
-    areThereMovesLeft = this.checkMovesLeftForWin([
-      this.board[3][0], 
-      this.board[2][1], 
-      this.board[1][2], 
-      this.board[0][3]
-    ]) || areThereMovesLeft;
+    areThereMovesLeft =
+      this.checkMovesLeftForWin([
+        this.board[3][0],
+        this.board[2][1],
+        this.board[1][2],
+        this.board[0][3],
+      ]) || areThereMovesLeft;
 
     // All Four Corners Check
-    areThereMovesLeft = this.checkMovesLeftForWin([
-      this.board[0][0], 
-      this.board[0][3], 
-      this.board[3][0], 
-      this.board[3][3]
-    ]) || areThereMovesLeft;
+    areThereMovesLeft =
+      this.checkMovesLeftForWin([
+        this.board[0][0],
+        this.board[0][3],
+        this.board[3][0],
+        this.board[3][3],
+      ]) || areThereMovesLeft;
 
     return areThereMovesLeft;
   }
 
   // Helper method used to determine if a win has occurred
   public winCheck(type: string, characters: string[]): Result {
-    if (characters[0] !== ' ' && 
-        characters[0] === characters[1] && 
-        characters[0] === characters[2] && 
-        characters[0] === characters[3]) {
+    if (
+      characters[0] !== " " &&
+      characters[0] === characters[1] &&
+      characters[0] === characters[2] &&
+      characters[0] === characters[3]
+    ) {
       this.result.win = true;
       this.result.type = type;
       this.result.character = characters[0];
     }
 
-    return this.result
+    return this.result;
   }
 
   // Helper Method to determine if a winning solution hasn't been blocked
   // returns false if any played character does not match the rest of the non-empty characters
   public checkMovesLeftForWin(characters: string[]): boolean {
-    let characterToFind = ' ';
+    let characterToFind = " ";
 
     for (let i = 0; i < characters.length; i++) {
-      if (characterToFind === ' ' && characters[i] !== ' ') {
+      if (characterToFind === " " && characters[i] !== " ") {
         characterToFind = characters[i];
       } else if (characters[i] !== characterToFind) {
         return false;
@@ -183,13 +201,13 @@ export class TicTacToe {
 
     return true;
   }
-  
+
   // Old method to determine if there are any moves left regardless of winning solution being blocked
-  // public anyMovesLeftRegardlessOfDraw(): boolean { 
+  // public anyMovesLeftRegardlessOfDraw(): boolean {
   //   if (this.checkWinner().win) {
   //     return false;
   //   }
-    
+
   //   for (let i = 0; i < 4; i++) {
   //     for (let j = 0; j < 3; j++) {
   //       if (this.board[i][j] === ' ') {
